267126b16a4af9f0563b263ac4df29b6c7f7bf44
morinokumasann <senlindexiongxiansheng@yahoo.co.jp>
2021-02-17 00:26:09 +0900

    Hi

---
+++ vue-element/src/views/Login.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <el-form ref="form" :model="form">
+            <el-form-item label="アカウント">
+                <el-input type="text" placeholder="アカウントを入力してください" v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item label='パスワード'>
+                <el-input type='password' placeholder="パスワードを入力してください" v-model="form.password"></el-input>
+            </el-form-item>
+        <el-form-item>
+            <el-button type="primary" @click="onSubmit">登録</el-button>
+        </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Login',
+    data(){
+        return{
+            form:{
+                name:'',
+                password:''
+
+            }
+
+        }
+
+    },
+    methods:{
+        onSubmit(){
+            alert("Hello")
+
+        }
+
+
+    }
+
+
+}
+</script>
+
+<style >
+
+</style>
